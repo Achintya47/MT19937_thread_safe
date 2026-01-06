caca82d1377f2c8c9b06e3b45c8fa9eeff239a11
Achintya Sharma <sharmaachintya49@gmail.com>
2026-01-06 19:23:37 +0530

    Refractored _index from int8 to int32. Implemented constructor logic for mt19937. Replaced dynamic _mt array with static allocated array

--- src/mt19937.cpp
+++ src/mt19937.cpp
@@ -2,18 +2,27 @@
 
 class mt19937{
     private:
-        uint32_t *_MT;
-        uint8_t _pos_index;
-        uint8_t _item;
+        uint32_t _mt[624];
+        uint16_t _index;
+        void twist();
+        static uint32_t temper(uint32_t);
     public:
-        mt19937();
-        ~mt19937();
+        mt19937(const uint32_t);
+        uint32_t next();
 };
 
 /*
-* @brief Constructor for 
-
+* @brief Constructor for growing and sowing seeds
+* @param seed Seed value used to populate the matrix
 */
-mt19937::mt19937(){
-
+mt19937::mt19937(const uint32_t seed){
+    _index = 624;
+    uint32_t _gseed = seed;
+    for (int i = 0; i < 51; i++){
+        _gseed = _gseed * 69069 + 1;
+    }
+    for (int i = 0; i < 624; i++){
+        _gseed = 69069 * _gseed + 1;
+        _mt[i] = _gseed & 0xFFFFFFFF;
+    }
 }
