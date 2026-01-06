8c129f7dac86c248bf27369bde2f394fc52a3813
Achintya Sharma <sharmaachintya49@gmail.com>
2026-01-06 20:19:32 +0530

    Implemented temper() function in mt19937.cpp and updated mt19937.hpp accordingly

--- src/mt19937.cpp
+++ src/mt19937.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cstdint>
+#include "mt19937.hpp"
+
 
 class mt19937{
     private:
@@ -7,11 +10,11 @@ class mt19937{
         void twist();
         static uint32_t temper(uint32_t);
     public:
-        mt19937(const uint32_t);
+        explicit mt19937(const uint32_t);
         uint32_t next();
 };
 
-/*
+/**
 * @brief Constructor for growing and sowing seeds
 * @param seed Seed value used to populate the matrix
 */
@@ -25,4 +28,19 @@ mt19937::mt19937(const uint32_t seed){
         _gseed = 69069 * _gseed + 1;
         _mt[i] = _gseed & 0xFFFFFFFF;
     }
+}
+
+/**
+ * @brief Temper function for standard MT19937
+ * 
+ * Called only by mt19937::next(), Stateless and independent
+ * of mt19937::twist()
+ */
+uint32_t mt19937::temper(uint32_t _mt0){
+    uint32_t y = _mt0;
+    y ^= (y >> 11) & MT_D;
+    y ^= (y << 5) & MT_B;
+    y ^= (y << 15) & MT_C;
+    y ^= y >> 18;
+    return y;
 }
